9cdcc157ab4032095f395e64755fa526b144c151
Roberto Ramirez <a01366943@itesm.mx>
2020-04-17 19:32:43 -0500

    Added front creation and consult of urls, landings and profiles. Login modified

--- dist/assets/demo/chart-area-demo.js
+++ dist/assets/demo/chart-area-demo.js
@@ -9,7 +9,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
     datasets: [{
-      label: "Sessions",
+      label: "Visitas",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
@@ -41,6 +41,57 @@ var myLineChart = new Chart(ctx, {
           min: 0,
           max: 40000,
           maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+
+var ctx = document.getElementById("myAreaChart2");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    datasets: [{
+      label: "Usos",
+      lineTension: 0.3,
+      backgroundColor: "rgba(247, 85, 120, 0.2)",
+      borderColor: "rgba(247, 85, 120, 1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(247, 85, 120, 1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(247, 85, 120, 1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [25347, 84257, 12354, 52344, 62342, 86753, 35244, 42135, 45673, 74843, 62341, 31984, 38451],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100000,
+          maxTicksLimit: 10
         },
         gridLines: {
           color: "rgba(0, 0, 0, .125)",
